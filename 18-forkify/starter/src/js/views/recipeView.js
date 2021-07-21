95dd764c2252d11cc8c449870b17fe23ff514325
Gagandeep Singh <gagandeepsinghjubbal@gmail.com>
2021-07-21 10:53:16 +0530

    Error handling

--- 18-forkify/starter/src/js/views/recipeView.js
+++ 18-forkify/starter/src/js/views/recipeView.js
@@ -5,6 +5,8 @@ import { Fraction } from 'fractional';
 class RecipeView {
   _parentElement = document.querySelector('.recipe');
   _data;
+  _errorMessage = "We couldn't find that recipe. Please try another one!";
+  _message = 'Start by searching for a recipe or an ingredient. Have fun!';
 
   render(data) {
     this._data = data;
@@ -17,7 +19,7 @@ class RecipeView {
     this._parentElement.innerHTML = '';
   }
 
-  renderSpinner = function () {
+  renderSpinner() {
     const markup = `
       <div class="spinner">
         <svg>
@@ -27,7 +29,37 @@ class RecipeView {
     `;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
-  };
+  }
+
+  renderError(message = this._errorMessage) {
+    const markup = `
+      <div class="error">
+      <div>
+        <svg>
+          <use href="${icons}#icon-alert-triangle"></use>
+        </svg>
+      </div>
+      <p>${message}</p>
+    </div>
+    `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderMessage(message = this._message) {
+    const markup = `
+      <div class="message">
+      <div>
+        <svg>
+          <use href="${icons}#icon-smile"></use>
+        </svg>
+      </div>
+      <p>${message}</p>
+    </div>
+    `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
 
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
